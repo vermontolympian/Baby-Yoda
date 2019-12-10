ac228be8e0a56eba61ec46232a3f8eeab3411bf8
2boog <2boog@130.215.12.64>
2019-12-10 16:49:15 -0500

    Created Left Insert

--- Left Insert.groovy
+++ Left Insert.groovy
@@ -1,1 +1,12 @@
-//Your code here
+File Right_Hand = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
+"DownRes-Left-Hand.stl");								
+CSG Right  = Vitamins.get(Right_Hand)						//Get hand STL
+			.scale(2)									//Scale to "life-like" size
+			.toXMin()
+			.toYMin()
+			.toZMin()
+
+CSG clearenceFit = Right.toolOffset(-2.0)
+			//.movex(50);
+
+return[clearenceFit];
