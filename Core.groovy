4209d5d1f924350bad693697ded577bf200a2141
2boog <2boog@130.215.14.15>
2020-02-06 11:32:14 -0500

    Flattened Bottom

--- Core.groovy
+++ Core.groovy
@@ -1,12 +1,13 @@
 File Left_Hand = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
 "DownRes-LeftInsert-4mm.stl");					
-CSG Left = Vitamins.get(Left_Hand)							//Get hand STL								//Scale to "life-like" size
+CSG Left = Vitamins.get(Left_Hand)							//Get hand STL	
 			.rotz(125)
 			.rotx(10)
 			.toXMin()
 			.toYMin()
 			.toZMin()
 			.movex(2)
+			.movez(-4)
 
 CSG Cylinder = new Cylinder (15,11,20,(int)30).toCSG()			//Create pouring slot geometry
 			.toZMax()
@@ -14,5 +15,12 @@ CSG Cylinder = new Cylinder (15,11,20,(int)30).toCSG()			//Create pouring slot g
 			.movex(15)
 			.movez(4.2)
 
+CSG Cube = new Cube (50,30,70).toCSG()
+			.toZMin()
+			.movex(18)
+			.movey(12.5)
 
-return[Left, Cylinder];
+CSG Hand = Left.intersect(Cube)
+
+
+return[Hand];
