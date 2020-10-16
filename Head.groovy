007dcc112aa36999dd130b226c72d98c02d779dd
Brandon Simpson <36552014+vermontolympian@users.noreply.github.com>
2020-10-16 13:09:04 -0400

    Created File

--- Head.groovy
+++ Head.groovy
@@ -1,1 +1,8 @@
-//Your code here
+File Head_stl = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
+"Head.stl");	
+CSG Head = Vitamins.get(Head_stl)
+			.toXMin()
+			.toYMin()
+			.toZMin()	
+
+return[Head];
