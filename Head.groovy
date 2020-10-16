911c91fde7a5f4b1325292f642dbf7e857807e1b
Brandon Simpson <36552014+vermontolympian@users.noreply.github.com>
2020-10-16 14:01:54 -0400

    Added front and back

--- Head.groovy
+++ Head.groovy
@@ -1,8 +1,22 @@
 File Head_stl = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
 "Head.stl");	
 CSG Head = Vitamins.get(Head_stl)
+			.rotx(9)
 			.toXMin()
 			.toYMin()
-			.toZMin()	
+			.toZMin()
 
-return[Head];
+CSG Cube1 = new Cube(200,100,160).toCSG()
+			.toXMin()
+			.toYMin()
+			.toZMin()
+			.movex(-20)
+			.movey(100)
+
+CSG Cube2 = new Cube(200,100,160).toCSG()
+			.toXMin()
+			.toYMin()
+			.toZMin()
+			.movex(-20)
+
+return[Head,Cube1,Cube2];
