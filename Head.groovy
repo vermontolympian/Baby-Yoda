0bf96d3707f75b4ef3b928c3e4ad5970cf3c7981
Brandon Simpson <36552014+vermontolympian@users.noreply.github.com>
2020-10-21 13:06:15 -0400

    Making Core

--- Head.groovy
+++ Head.groovy
@@ -1,32 +1,41 @@
 File Head_stl = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
 "Head.stl");	
 CSG Head = Vitamins.get(Head_stl)
+			.toXMin()
+			.toYMin()
+			.toZMin()
 			.rotx(9)
-			.toXMin()
-			.toYMin()
-			.toZMin()
+			.movez(-2)
 
-CSG Cube1 = new Cube(200,100,160).toCSG()
+//File Eyes_stl = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
+//"Eyes.stl");	
+//CSG Eyes = Vitamins.get(Eyes_stl)
+			.rotx(9)
+			
+
+//File Ears_stl = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
+//"Ears.stl");	
+//CSG Ears = Vitamins.get(Ears_stl)
+//			.rotx(9)
+
+
+CSG Side1 = new Cube(200,200,160).toCSG()
 			.toXMin()
 			.toYMin()
 			.toZMin()
 			.movex(-20)
-			.movey(100)
-
-CSG Cube2 = new Cube(200,100,160).toCSG()
-			.toXMin()
-			.toYMin()
-			.toZMin()
-			.movex(-20)
-
-CSG Back = Cube1.difference(Head)
-			.rotx(-90)
-			.toZMin()
-			.movey(20)
-
-//CSG Front = Cube2.difference(Head)
-//			.rotx(90)
+			
+//CSG Side2 = new Cube(200,100,160).toCSG()
+//			.toXMin()
+//			.toYMin()
 //			.toZMin()
-//			.movey(-20)
+//			.movex(-20)
+//			.movey(100)
 
-return[Back];
+
+def stuff = [Head,Ears,Eyes];
+
+CSG Front = Side1.difference(Head);
+//CSG Back = Side2.difference(Head);
+
+return[Front];
